7d642c88a856a8e5c9e9e08e0c7ab986feb048ef
Dua Lipa <quentin.vedrenne74@gmail.com>
2022-10-06 14:46:59 +0200

    ADD: New style with max review

--- src/Story.js
+++ src/Story.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import { motion } from 'framer-motion'
+import Image from 'next/image';
 
 
 export const storyAnim = {
@@ -19,24 +20,8 @@ export const storyAnim = {
 };
 
 const Story = () => {
-  
-  const meAnim = {
-    offscreen: {
-      opacity: 0,
-      y: 0,
-    },
-    onscreen: {
-      y: 25,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 2
-      }
-    }
-  };
 
-  const aboutAnim = {
+  const aboutmeAnim = {
     offscreen: {
       opacity: 0,
       y: 0,
@@ -63,16 +48,25 @@ const Story = () => {
         viewport={{ once: true, amount: 0.4 }}
       >
         <div className={styles.aboutme}>
-          <motion.div variants={aboutAnim} className={styles.titleStory}> About</motion.div>
-          <motion.div variants={meAnim} className={styles.titleStory} >Me</motion.div>
-        </div>
-        <div className={styles.story}>
-          <motion.div variants={storyAnim}>
-          Hi, my name is Quentin Vedrenne I’m a French Student from EPITECH and I’m researching an intern position as Software Engineer to conclude my fifth and last year of school from March to August 2023. With now three years in the IT development and 1 year in the role of project manager I’m confident that my knowledge will be a real asset to the team.
+          <div className={styles.containerImg}>
+            <div className={styles.nextImg}>
+              <Image width="250" height="260" src='/pp.jpeg' alt='profile picture' />
+            </div>
+          </div>
+          <motion.div variants={aboutmeAnim} className={styles.titleStory}>
+            <p>About me</p>
           </motion.div>
         </div>
-        {/* <p className={styles.lighttextwrapper}>d<p id="lighttext" className={styles.lighttext}>About</p></p> */}
-        {/* <span className={styles.boldtextwrapper}><span id="boldtext" className={styles.boldtext}>Memories</span></span> */}
+        <div className={styles.textContainer}>
+          <motion.div variants={storyAnim}>
+            <p>
+              Hi, my name is Quentin Vedrenne I’m a French Student from EPITECH and I’m researching an intern position as Software Engineer to conclude my fifth and last year of school from March to August 2023. With now three years in the IT development and 1 year in the role of project manager I’m confident that my knowledge will be a real asset to the team.
+            </p>
+            <p>
+              skdkdk
+            </p>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   )
--- src/timelineElements.js
+++ src/timelineElements.js
@@ -1,20 +1,21 @@
 let timelineElements = [
   {
     id: 1,
-    title: "Project manager / FullStack developper - JOB",
+    title: "Co-Founder / Lead developer",
     type: "experience",
-    company_name: "Rubrash",
+    company_name: "Rubrash - JOB",
     location: "Lyon, France",
-    date: "2020 - 2021 (15 MONTHS)",
-    skills: ["project managment", "NextJS", "NodeJS" ],
+    date: "2020 - 2021 (15 Months)",
+    skills: ["Project managment", "NextJS", "NodeJS", "Lead developer"],
     detail: [{
       id: 1,
-      title: "Project Manager",
+      title: "Project manager",
       task: ["Design and Development of a newspaper website with a microservice architecture. (Javascript / Scrum)", "Conception, Development, and Growth hacking for some market places. (Laravel / Scrum)"]
     },
     {
       id: 2,
-      title: "Lead developper (Next.JS / nodeJS)",
+      title: "Lead developer",
+      task: ["Maintain and add features to a newspaper website (NextJS / NodeJS)"]
     }
     ],
   },
@@ -24,15 +25,14 @@ let timelineElements = [
     type: "experience",
     company_name: "Tribune de Lyon",
     location: "Lyon, France",
+    skills: ["Scrum Master", "Wordpress", "Newpaper"],
     detail: [{
       id: 1,
       title: "Scrum Master for newspapers website",
-    },
-    {
-      id: 2,
-      title: "Made a link between the journalist and the development team for rebuilt the back office",
+      task: ["Made a link between the journalist and the development team for rebuilt the back office" , "Maintain and add features for the all newpaper websites"]
+
     }],
-    date: "2020-2021 (8 MONTHS)",
+    date: "2020-2021 (8 Months)",
   },
   {
     id: 3,
@@ -40,15 +40,13 @@ let timelineElements = [
     type: "experience",
     company_name: "Capgemini",
     location: "Lyon, France",
+    skills: ["Scrum", "Angular", "Typescript"],
     detail: [{
       id: 1,
       title: "Agile Software Integrator",
-    },
-    {
-      id: 2,
-      title: "Developper full stack for a platform to predict the impact of the stock market thanks to social networks (Angular)",
+      task: ["Developer full stack for a platform to predict the impact of the stock market thanks to social networks (Angular)"]
     }],
-    date: "2018 - 2019 (4 MONTHS)",
+    date: "2018 - 2019 (4 Months)",
   },
   {
     id: 4,
@@ -56,6 +54,7 @@ let timelineElements = [
     type: "experience",
     location: "Geneva, Swirtzeland",
     company_name: "Clinique Generale Beaulieu",
+    skills: ["VBA", "Managment Process", "Typescript"],
     detail: [{
       id: 1,
       title: "Developed a program to automate the statistics of goods received at the Clinic.",
@@ -64,18 +63,25 @@ let timelineElements = [
       id: 2,
       title: "Participated in the development of an automated supplier statistics program that shows the order management activity and the financial aspect of the department",
     }],
-        date: "2018-2019 (2 MONTHS)",
+        date: "2018-2019 (2 Months)",
   },
   {
     id: 5,
     title: "Computer science / Engineer",
     location: "San Francisco, United States",
     type: "Education",
+    skills: ["Software Engineer", "React", "NodeJS", "Typescript", "Machine Learning", "Biologie"],
     company_name: "SAN FRANCISCO STATE UNIVERSITY - EXCHANGE STUDENT",
     detail: [{
       id: 1,
-      title: "Fall 2021 and Spring 2022 / 3.5 GPA",
-    }],
+      title: "Fall 2021 (Computer Science) / 3.6 GPA",
+    },
+    {
+      id: 2,
+      title: "Spring 2022 (Computer Science - Engineer) / 3.5 GPA",
+
+    }
+  ],
     date: "2021 - 2022 (1 Year)",
   },
   {
@@ -83,6 +89,7 @@ let timelineElements = [
     title: "Programme Grande Ecole",
     company_name: "EPITECH - MASTER OF COMPUTER SCIENCE",
     location: "Paris, France",
+    skills: ["Software Engineer", "C", "C++", "React", "NodeJS", "Docker", "DevOps", "Kotlyn"],
     detail: [{
       id: 1,
       title: "Development of a web platform to create a special workflow for standard (OnGoing - React / Golang)",
