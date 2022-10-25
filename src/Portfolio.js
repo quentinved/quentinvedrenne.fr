7a5b07eca2b1500745c5b4e314156c60615a1b9f
Dua Lipa <quentin.vedrenne74@gmail.com>
2022-10-25 19:20:26 +0200

    ADD: responsive for portfolio and fix iphone pb

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -9,9 +9,9 @@ const Slide = (props) => {
     return (
         <div className={styles[props.class]}>
             <div className={styles.textPortfolio}>
-                    <h1 className={styles.titlePortfolioSlide}>
-                        {props.data.title}
-                    </h1>
+                <h1 className={styles.titlePortfolioSlide}>
+                    {props.data.title}
+                </h1>
                 <div className={styles.containerSkillsPortfolio}>
                     {props.data.skills && props.data.skills.map((elem, index) => {
                         return <h3 className={styles[props.classSkils]} key={index}>{elem}</h3>
@@ -22,7 +22,7 @@ const Slide = (props) => {
                 <p className={styles.descriptionTitlePortfolio}>My Jobs</p>
                 {props.data.detail?.map((elem, index) => {
                     return (
-                        <ul key={index}  className={styles.descriptionPortfolio}>
+                        <ul key={index} className={styles.descriptionPortfolio}>
                             <li> {elem.title}
                                 <ul>
                                     {elem.task?.map((task, index) => {
@@ -36,7 +36,11 @@ const Slide = (props) => {
             </div>
             <div className={styles.rightSlide}>
                 {props.data.img && props.data.img.map((img, index) => {
-                    return (<Image src={img} width={600} height={400} key={index} alt={props.data.title} className={styles.imgPortfolio} />)
+                    return (
+                    <div className={styles.imgPortfolioContainer} key={index}>
+                        <Image src={img} key={index} alt={props.data.title} className={styles.imgPortfolio} layout='fill' />
+                    </div>
+                    )
                 })
                 }
             </div>
