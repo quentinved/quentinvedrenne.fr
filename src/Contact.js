20a4e7c82636d9304372a3f766c94680eba7eeb2
Quentin Vedrenne <58831675+quentinved@users.noreply.github.com>
2022-11-13 16:48:45 +0100

    Change font + add contact part with responsive

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,33 @@
 import styles from "../styles/Home.module.css"
+import Image from "next/image"
+import { PopupButton } from '@typeform/embed-react'
+
+const Typeform = () => {
+    return <PopupButton id="xjazXMlj" style={{ fontSize: 20 }} className={styles.button64} >
+        <span>
+            Fill in the form to contact me 👀
+        </span>
+    </PopupButton>
+}
 
 const Contact = () => {
     return (
         <div className={styles.contactContainer}>
-            <h3> Still There ? </h3>
-            <p>to contact me ...</p>
+            <div className={styles.contactTitleContainer}>
+                <h3 className={styles.titleContact}> Contact </h3>
+            </div>
+            <div className={styles.typeformContainer}>
+                <p className={styles.stillthereContact}>Still There ?</p>
+                <Typeform />
+                <div className={styles.socialnetworkContainer}>
+                    <div className={styles.socialnetworkImg}>
+                        <Image src="/github.png" alt="arrow" layout="fill" className={styles.socialnetworkImg} />
+                    </div>
+                    <div className={styles.socialnetworkImg}>
+                        <Image src="/linkedin.png" alt="arrow" layout="fill" className={styles.socialnetworkImg} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
