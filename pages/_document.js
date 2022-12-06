e268f3632683946938f7deeb161ca5b45ffed45a
Dua Lipa <quentin.vedrenne74@gmail.com>
2022-12-06 18:02:34 +0100

    Patch warding and some reduce some widht

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          // href="https://fonts.googleapis.com/css2?family=GEMUNU+LIBRE&display=optional"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
